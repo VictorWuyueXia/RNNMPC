6d5fc4a6bcd4371cad5ccc5f8feefa62d01c1b75
Victor Xia <victor.w.x@icloud.com>
2024-02-03 03:40:28 -0500

    2024/02/03 3:40am

---
+++ Controllers/MPC/CostWeigts.m
@@ -0,0 +1,20 @@
+classdef CostWeigts
+    % Weights of cost function terms
+
+    properties
+        input
+        progress
+        laneKeeping
+        obsAvoid
+        RL
+        softConstraintPenalty
+        terminalState
+    end
+
+    methods
+        function obj = CostWeigts()
+
+        end
+
+    end
+end
